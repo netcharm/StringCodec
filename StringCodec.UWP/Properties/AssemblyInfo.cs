bfecfd5020cc57ce1b5ae5c6abc832ac682434d4
netcharm <netcharm@163.com>
2018-08-16 23:40:11 +0800

    Add NavigationView Pane

---
+++ StringCodec.UWP/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringCodec.UWP")]
+[assembly: AssemblyDescription("String Encode & Decode")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NetCharm Studio")]
+[assembly: AssemblyProduct("StringCodec.UWP")]
+[assembly: AssemblyCopyright("Copyright © NetCharm Studio 2018")]
+[assembly: AssemblyTrademark("NetCharm")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
